84f0dfc92a124ce51aaf84190a560b07ba1791ae
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-02 15:50:21 +0900

    モデルの単体テストコードを入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,18 @@ class User < ApplicationRecord
     validates :family_name_katakana
     validates :first_name_katakana
     validates :birthday
+    
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :family_name
+      validates :first_name
+    end
+
+      with_options format: { with: /\A[ァ-ヶ一]+\z/ } do
+        validates :family_name_katakana
+        validates :first_name_katakana
+    end
   end
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
