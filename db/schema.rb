60ba0294c7d812e3c7adc6163c4d7d0d74c06f1e
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-04 18:11:19 +0900

    商品一覧表示機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_145016) do
+ActiveRecord::Schema.define(version: 2022_05_04_082839) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 2022_05_03_145016) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "purchases_histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchases_histories_on_item_id"
+    t.index ["user_id"], name: "index_purchases_histories_on_user_id"
+  end
+
   create_table "shipping_areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -93,4 +102,6 @@ ActiveRecord::Schema.define(version: 2022_05_03_145016) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "purchases_histories", "items"
+  add_foreign_key "purchases_histories", "users"
 end
